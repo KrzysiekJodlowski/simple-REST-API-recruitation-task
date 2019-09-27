2aa25d9609ffdfd0f0538ad392c7d5d056422449
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-09-27 10:17:24 +0200

    Initialize Spring project with Maven

---
+++ src/main/java/com/jodlowski/simple_rest_api_recruitation_task/SimpleRestApiRecruitationTaskApplication.java
@@ -0,0 +1,13 @@
+package com.jodlowski.simple_rest_api_recruitation_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApiRecruitationTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestApiRecruitationTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jodlowski/simple_rest_api_recruitation_task/SimpleRestApiRecruitationTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jodlowski.simple_rest_api_recruitation_task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleRestApiRecruitationTaskApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
